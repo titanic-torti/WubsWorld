b2630b21c699fd8e92d9654322c75bbf9ef67e37
Austin Tepe <atepe101@gmail.com>
2025-02-13 02:03:55 -0800

    button interactivity main menu screen

---
+++ Assets/Scripts/ButtonInterface.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonInterface : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ViewCredits()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
