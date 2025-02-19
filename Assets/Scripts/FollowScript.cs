9546ec09167c42bd0d9e3a6d58d40cae40206d51
Austin Tepe <atepe101@gmail.com>
2025-02-19 15:30:11 -0800

    army of crabs

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -2,18 +2,40 @@ using UnityEngine;
 
 public class FollowScript : MonoBehaviour
 {
+    [Header("SFX")]
+    [SerializeField] AudioSource soundCrabHurt;
+    [SerializeField] AudioSource soundCrabStep;
+    [SerializeField] float crabStepTimerSet;
+    private float _crabStepTimer;
+
     [SerializeField] Transform player;      // position of player
     [SerializeField] float moveSpeed;       // move speed of enemy
+
+    void Start()
+    {
+        _crabStepTimer = crabStepTimerSet;
+    }
 
     void Update()
     {
         transform.position += new Vector3((player.position-transform.position).normalized.x, 0, 0) * moveSpeed * Time.deltaTime;
+
+        if (_crabStepTimer <= 0)
+        {
+            soundCrabStep.Play();
+            _crabStepTimer = crabStepTimerSet;
+        }
+        else
+        {
+            _crabStepTimer -= Time.deltaTime;
+        }
     }
 
     void OnCollisionEnter2D (Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Anchor"))
         {
+            soundCrabHurt.Play();
             Destroy(gameObject);
         }
     }
