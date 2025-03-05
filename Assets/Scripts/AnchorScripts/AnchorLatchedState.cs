519c5a47fc8276d0425bd94ee84fbea566402121
Connor S <csull54@gmail.com>
2025-03-04 21:24:27 -0800

    Implemented extending and retracting anchor, changed it to allow walking around when latched

--- Assets/Scripts/AnchorScripts/AnchorLatchedState.cs
+++ Assets/Scripts/AnchorScripts/AnchorLatchedState.cs
@@ -7,7 +7,7 @@ public class AnchorLatchedState : AnchorBaseState
         anchor._rb.simulated = true;
         anchor._rb.bodyType = RigidbodyType2D.Kinematic;
         anchor._rb.constraints = RigidbodyConstraints2D.FreezePosition;
-        anchor._dj.enabled = true;
+        //anchor._dj.enabled = true;
     }
 
     void RevertEnterStateChanges(AnchorStateManager anchor)
@@ -19,6 +19,7 @@ public class AnchorLatchedState : AnchorBaseState
 
     public override void UpdateState(AnchorStateManager anchor)
     {
+        // Detaching anchor
         float retrieveInput = anchor._hookRetrieve.ReadValue<float>();
         if (retrieveInput > 0)
         {
@@ -26,11 +27,38 @@ public class AnchorLatchedState : AnchorBaseState
             anchor._latchTimer = anchor.timeRecoverFromLatch;
             anchor.SwitchState(anchor.IdleState);
         }
+
+        if (!anchor.playerScript.jumpCheckScript.IsGrounded())
+        {
+            const float ADJUSTMENT_SPEED = 0.01f;
+
+            // Are these keys okay? Do they feel "natural"?
+            const KeyCode EXTEND_KEY = KeyCode.S;
+            const KeyCode RETRACT_KEY = KeyCode.W;
+
+            // Extend
+            if (Input.GetKey(EXTEND_KEY) && anchor._dj.distance < anchor.maxAnchorDist)
+            {
+                anchor._dj.distance += ADJUSTMENT_SPEED;
+            }
+
+            // Retract
+            if (Input.GetKey(RETRACT_KEY) && anchor._dj.distance > 1)
+            {
+                anchor._dj.distance -= ADJUSTMENT_SPEED;
+            }
+        }
     }
 
     public override void FixedUpdateState(AnchorStateManager anchor)
     {
+        if (anchor.playerScript.jumpCheckScript.IsGrounded() && anchor._dj.enabled) {
+            anchor._dj.enabled = false;
+        }
 
+        if (!anchor.playerScript.jumpCheckScript.IsGrounded() && !anchor._dj.enabled) {
+            anchor._dj.enabled = true;
+        }
     }
 
     public override void OnCollisionEnter2D(AnchorStateManager anchor, Collision2D collision)
