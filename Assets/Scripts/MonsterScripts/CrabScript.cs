e9e7da6bb69c65f72c89fe58a05ddbc20ae40403
Austin Tepe <atepe101@gmail.com>
2025-03-13 00:42:47 -0700

    BlobFish added as enemy

--- Assets/Scripts/MonsterScripts/CrabScript.cs
+++ Assets/Scripts/MonsterScripts/CrabScript.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class FollowScript : MonoBehaviour
 {
-    [Header("SFX")]
-    [SerializeField] AudioSource soundCrabStep;
-    [SerializeField] float crabStepTimerSet;
-    private float _crabStepTimer;
+    // [Header("SFX")]
+    // [SerializeField] AudioSource soundCrabStep;
+    // [SerializeField] float crabStepTimerSet;
+    // private float _crabStepTimer;
 
     [Header("Player Feedback")]
     [SerializeField] Transform player;      // position of player
@@ -14,10 +14,10 @@ public class FollowScript : MonoBehaviour
     [Header("Crab Behavior")]
     [SerializeField] float moveSpeed;       // move speed of enemy
 
-    void Start()
-    {
-        _crabStepTimer = crabStepTimerSet;
-    }
+    // void Start()
+    // {
+    //     _crabStepTimer = crabStepTimerSet;
+    // }
 
     void Update()
     {
@@ -27,15 +27,15 @@ public class FollowScript : MonoBehaviour
             transform.position += moveSpeed * Time.deltaTime * new Vector3((player.position-transform.position).normalized.x, 0, 0);
         }
 
-        if (_crabStepTimer <= 0 && !soundCrabStep.isPlaying)
-        {
-            soundCrabStep.Play();
-            _crabStepTimer = crabStepTimerSet;
-        }
-        else
-        {
-            _crabStepTimer -= Time.deltaTime;
-        }
+        // if (_crabStepTimer <= 0 && !soundCrabStep.isPlaying)
+        // {
+        //     soundCrabStep.Play();
+        //     _crabStepTimer = crabStepTimerSet;
+        // }
+        // else
+        // {
+        //     _crabStepTimer -= Time.deltaTime;
+        // }
     }
 
     void OnCollisionEnter2D (Collision2D collision)
